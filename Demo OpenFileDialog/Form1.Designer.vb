e3da4c486cb1aaf3017f78426712a110f3165937
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2020-07-29 09:01:00 +0700

    Add project files.

---
+++ Demo OpenFileDialog/Form1.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ofdOpenFile = New System.Windows.Forms.OpenFileDialog()
+        Me.BtnOpenDialog = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ofdOpenFile
+        '
+        Me.ofdOpenFile.FileName = "OpenFileDialog1"
+        '
+        'BtnOpenDialog
+        '
+        Me.BtnOpenDialog.Location = New System.Drawing.Point(58, 91)
+        Me.BtnOpenDialog.Name = "BtnOpenDialog"
+        Me.BtnOpenDialog.Size = New System.Drawing.Size(163, 39)
+        Me.BtnOpenDialog.TabIndex = 0
+        Me.BtnOpenDialog.Text = "Buka Open Dialog"
+        Me.BtnOpenDialog.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.BtnOpenDialog)
+        Me.Name = "Form1"
+        Me.Text = "Demo OpenFileDialog"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ofdOpenFile As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents BtnOpenDialog As Button
+End Class
---
+++ Demo OpenFileDialog/Form1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BtnOpenDialog_Click(sender As Object, e As EventArgs) Handles BtnOpenDialog.Click
+        'mengkonfigurasi kotak dialog Open dan menampilkannya dengan 
+        With ofdOpenFile
+            .Filter = "File teks (*.txt)|*.txt|Semua file (*.*)|*.*"
+            .InitialDirectory = "C:\Data"
+            .Title = "Pilih File yang Akan Dibuka"
+
+            If .ShowDialog() = Windows.Forms.DialogResult.OK Then
+                MessageBox.Show(ofdOpenFile.FileName)
+            Else
+                MessageBox.Show("Anda tidak memilih file apapun.")
+
+            End If
+        End With
+    End Sub
+End Class
